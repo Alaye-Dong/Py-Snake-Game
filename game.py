0d6b43d2029b6898e137f6aaf89ca2d6486620fa
Alaye-Dong <AlayeVast@outlook.com>
2024-12-24 21:16:46 +0800

    refactor: 🦄 分离代码

---
+++ game.py
@@ -0,0 +1,50 @@
+import pygame
+from snake import snake
+from cube import cube
+from utils import drawGrid, randomSnack
+
+width = 500
+height = 500
+cols = 25
+rows = 20
+
+def redrawWindow():
+    global win
+    win.fill((0,0,0))
+    drawGrid(width, rows, win)
+    s.draw(win)
+    snack.draw(win)
+    pygame.display.update()
+
+def main():
+    global s, snack, win
+    win = pygame.display.set_mode((width,height))
+    s = snake((255,0,0), (10,10))
+    s.addCube()
+    snack = cube(randomSnack(rows,s), color=(0,255,0))
+    flag = True
+    clock = pygame.time.Clock()
+    
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        s.move()
+        headPos = s.head.pos
+        if headPos[0] >= 20 or headPos[0] < 0 or headPos[1] >= 20 or headPos[1] < 0:
+            print("Score:", len(s.body))
+            s.reset((10, 10))
+
+        if s.body[0].pos == snack.pos:
+            s.addCube()
+            snack = cube(randomSnack(rows,s), color=(0,255,0))
+            
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
+                print("Score:", len(s.body))
+                s.reset((10,10))
+                break
+                    
+        redrawWindow()
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,28 @@
+import pygame
+import random
+from cube import cube
+
+def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y +sizeBtwn
+
+        pygame.draw.line(surface, (255,255,255), (x, 0),(x,w))
+        pygame.draw.line(surface, (255,255,255), (0, y),(w,y))
+
+def randomSnack(rows, item):
+    positions = item.body
+
+    while True:
+        x = random.randrange(1,rows-1)
+        y = random.randrange(1,rows-1)
+        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
+               continue
+        else:
+               break
+
+    return (x,y)
